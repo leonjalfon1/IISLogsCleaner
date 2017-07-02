26eb9280b4deed411bc08b9c43a3d4dabc5351b0
leonj <leon.jalfon@checkmarx.com>
2017-07-02 16:22:06 +0300

    Add IISLogsCleaner.ps1 script

---
+++ IISLogsCleaner.ps1
@@ -0,0 +1,32 @@
+﻿# Script to cleanup IIS log files 
+# By Default: Remove logs files located in "C:\inetpub\logs" greater than 30 days old. 
+
+
+# CONFIGURATION
+
+$LogPath = "C:\inetpub\logs" 
+$maxDaystoKeep = -30
+ 
+
+# FUNCTION
+
+$itemsToDelete = dir $LogPath -Recurse -File *.log | Where LastWriteTime -lt ((get-date).AddDays($maxDaystoKeep)) 
+$totalFiles = $itemsToDelete.Count
+
+if ($itemsToDelete.Count -gt 0)
+{ 
+    ForEach ($item in $itemsToDelete)
+    { 
+        Get-item $item.FullName | Remove-Item -Verbose 
+    } 
+
+    Write-Output "Cleanup of log files older than $((get-date).AddDays($maxDaystoKeep)) completed..." 
+    Write-Output "$totalFiles Files Deleted..." 
+} 
+else
+{ 
+    Write-Output "No items to be deleted today $($(Get-Date).DateTime)" 
+} 
+   
+
+start-sleep -Seconds 10
